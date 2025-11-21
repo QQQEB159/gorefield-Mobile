18f955cb2b230cb1da0c8f9a16e08217d2a713ae
tchcfhvgd <2220687506@qq.com>
2025-11-21 14:59:12 +0800

    lol

--- source/mobile/funkin/backend/utils/MobileData.hx
+++ source/mobile/funkin/backend/utils/MobileData.hx
@@ -46,7 +46,7 @@ class MobileData
 	public static function init()
 	{
 		save = new FlxSave();
-		save.bind('MobileControls', #if sys 'YoshiCrafter29/CodenameEngine' #else 'CodenameEngine' #end);
+		save.bind('qqqeb', #if sys 'YoshiCrafter29/CodenameEngine' #else 'CodenameEngine' #end);
 		
 		for (folder in [
 			'${ModsFolder.modsPath}${ModsFolder.currentModFolder}/mobile',
