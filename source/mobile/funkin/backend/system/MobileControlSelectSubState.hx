5bd58a962737c8f7c236e71cfdbe5df5df9f741a
tchcfhvgd <2220687506@qq.com>
2025-11-30 08:04:57 +0800

    test

--- source/mobile/funkin/backend/system/MobileControlSelectSubState.hx
+++ source/mobile/funkin/backend/system/MobileControlSelectSubState.hx
@@ -210,6 +210,46 @@ class MobileControlSelectSubState extends MusicBeatSubstate
 						moveButton(TouchUtil.touch, button);
 				});
 			}
+			control.touchPad.forEachAlive((button:TouchButton) ->
+			{
+				if (button != bindButton && buttonBinded)
+				{
+					bindButton.centerBounds();
+					button.bounds.immovable = true;
+					bindButton.bounds.immovable = false;
+					button.centerBounds();
+					FlxG.overlap(bindButton.bounds, button.bounds, function(a:Dynamic, b:Dynamic)
+					{ // these args dosen't work fuck them :/
+						bindButton.centerInBounds();
+						button.centerBounds();
+						bindButton.bounds.immovable = true;
+						button.bounds.immovable = false;
+						// trace('button${bindButton.tag} & button${button.tag} collided');
+					}, function(a:Dynamic, b:Dynamic)
+					{
+						if (!bindButton.bounds.immovable)
+						{
+							if (bindButton.bounds.x > button.bounds.x)
+								bindButton.bounds.x = button.bounds.x + button.bounds.width;
+							else
+								bindButton.bounds.x = button.bounds.x - button.bounds.width;
+
+							if (bindButton.bounds.y > button.bounds.y)
+								bindButton.bounds.y = button.bounds.y + button.bounds.height;
+							else if (bindButton.bounds.y != button.bounds.y)
+								bindButton.bounds.y = button.bounds.y - button.bounds.height;
+						}
+						return true;
+					});
+					/*FlxG.collide(bindButton.bounds, button.bounds, function(a:Dynamic, b:Dynamic) { // these args dosen't work fuck them :/
+						bindButton.centerInBounds();
+						button.centerBounds();
+						bindButton.bounds.immovable = true;
+						button.bounds.immovable = false;
+						trace('button${bindButton.tag} & button${button.tag} collided');
+					});*/
+				}
+			});
 		}
 
 		tweenieShit += 180 * elapsed;
@@ -316,8 +356,8 @@ class MobileControlSelectSubState extends MusicBeatSubstate
 	{
 		bindButton = button;
 		buttonBinded = bindButton == null ? false : true;
-		bindButton.x = touch.x - Std.int(bindButton.width / 2);
-		bindButton.y = touch.y - Std.int(bindButton.height / 2);
+		bindButton.x = touch.getScreenPosition(ui).x - Std.int(bindButton.width / 2);
+		bindButton.y = touch.getScreenPosition(ui).y - Std.int(bindButton.height / 2);
 		updatePosText();
 	}
 }
